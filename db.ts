53b969d7e3a66a6e81ca5dbeaaa0c063db221b3a
sheryak_n <nathanraysheryak@gmail.com>
2022-12-20 07:39:22 -0500

    open db connection + /ping route to test connection

--- db.ts
+++ db.ts
@@ -1,10 +1,16 @@
-import { Pool } from "pg"
+import { Client } from "pg"
 
-export default new Pool({
-  max: 20,
-  idleTimeoutMillis: 30000,
+export const client = new Client({
   user: "docker",
   host: "db",
   password: "1234",
   port: 5432,
+})
+
+client.connect((error) => {
+  if (error) {
+    console.log("DB connection error: ", error)
+  } else {
+    console.log("DB connected")
+  }
 })
--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 import express, { Express, Request, Response } from "express"
 
+import { client } from "./db"
+
 const PORT = 5000
 
 const app: Express = express()
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response): void => {
   res.send("Jello, Tracker!")
+})
+
+app.get("/ping", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const result = await client.query("SELECT NOW();")
+    res.send("RESULT! " + result)
+  } catch (error: any) {
+    res.send("CATCH " + error.message)
+  }
 })
 
 app.listen(PORT, () => {
